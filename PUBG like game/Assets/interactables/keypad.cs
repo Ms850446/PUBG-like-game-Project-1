fbbec3f8a6de003fb16ee85d11f3c1043cafe6f8
ErenyEleya <81209944+ErenyEleya@users.noreply.github.com>
2024-02-08 10:28:15 +0200

    celebration

--- PUBG like game/Assets/interactables/keypad.cs
+++ PUBG like game/Assets/interactables/keypad.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class keypad : Interactables
 {
     [SerializeField]
     private GameObject door;
     private bool doorOpen;
+    public ParticleSystem celebration;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +28,12 @@ public class keypad : Interactables
         // Debug.Log("interacted with"+gameObject.name);
         doorOpen = !doorOpen;
         door.GetComponent<Animator>().SetBool("isOpen",doorOpen);
+        if(doorOpen){
+            PlayWinningEffect();
+        }
+    }
+    void PlayWinningEffect()
+    {
+        celebration.Play();
     }
 }
